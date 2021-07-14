ed3eddde7940755f22809260f2950a99f67a8360
kelliewatson <kelliewatson@gmail.com>
2021-07-13 17:01:07 -0700

    Added event listener to adjust window size. Code implemented from code in Pluralsight tutorial.
    Tutorial: Rerender a react component on window resize by Jake Trent 6 min read 2020/10/20
    
    https://app.pluralsight.com/guides/re-render-react-component-on-window-resize
    
    Code Example:
    https://codesandbox.io/s/condescending-https-z6fmh?resolutionWidth=1541&resolutionHeight=675&file=/src/index.js:87-861

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './style/App.css';
 import Resume from './Resume.js';
 import {Container} from "react-bootstrap";
@@ -7,15 +10,44 @@ import Header from "./Header"
 
 
 function App() {
+    const [dimensions, setDimensions] = React.useState({
+        height: window.innerHeight,
+        width: window.innerWidth
+    });
+    React.useEffect(() => {
+        const debouncedHandleResize = debounce(function handleResize() {
+            setDimensions({
+                height: window.innerHeight,
+                width: window.innerWidth
+            });
+        }, 1000);
 
+        window.addEventListener("resize", debouncedHandleResize);
 
-  return (
-    <Container className="App" fluid>
-        <Header />
-        <Resume />
-        <Footer/>
+        return _ => {
+            window.removeEventListener("resize", debouncedHandleResize);
+        };
+    });
+    return (
+        <Container className="App" fluid>
+            <Header />
+            <Resume />
+            <Footer/>
 
-    </Container>
-  );
+        </Container>
+
+    );
+}
+
+function debounce(fn, ms) {
+    let timer;
+    return _ => {
+        clearTimeout(timer);
+        timer = setTimeout(_ => {
+            timer = null;
+            fn.apply(this, arguments);
+        }, ms);
+    };
+
 }
 export default App;
