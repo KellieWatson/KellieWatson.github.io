ca19b48d8cd4533234fe5d7e88ef84ae5a163ccd
kelliewatson <kelliewatson@gmail.com>
2021-07-14 15:36:59 -0700

    changed Qualification bullets again

--- src/Qualifications.js
+++ src/Qualifications.js
@@ -2,11 +2,11 @@ import './style/Qualifications.css'
 import {ListGroup} from "react-bootstrap";
 
 function Qualifications(){
- const qualifications = [   "Proficiency with Java SE and the Object Oriented Paradigm ",
-                            "Fundamental knowledge of a variety of programming languages: Javascript C, C++, Python, SQL ",
+ const qualifications = [   "Proficiency with Java SE and the Object Oriented paradigm ",
+                            "Experience with React and the NextJS Framework ",
                             "Familiarity with Git's distributed version control",
-                            "Excellent communication skills, both written and verbal",
                             "Adept facilitator of group discussion and negotiations ",
+                            "Excellent communication skills, both written and verbal",
                             "Strong analytical and troubleshooting capabilities",
                             "Eager contributor of production focused efforts"];
 
