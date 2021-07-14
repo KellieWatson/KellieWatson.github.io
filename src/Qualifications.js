6210a5bbdc3866d3cb79f5831ce6396c8b6888a4
kelliewatson <kelliewatson@gmail.com>
2021-07-14 14:41:35 -0700

    added Qualification bullet

--- src/Qualifications.js
+++ src/Qualifications.js
@@ -2,12 +2,13 @@ import './style/Qualifications.css'
 import {ListGroup} from "react-bootstrap";
 
 function Qualifications(){
- const qualifications = [   "Proficiency with Object Oriented Programming",
-                            "Fundamental knowledge of Requirement Engineering and the Software Design Lifecycle ",
-                            "Familiarity with Distributed Software Solutions",
+ const qualifications = [   "Proficiency with Java SE and the Object Oriented Paradigm ",
+                            "Fundamental knowledge of a variety of programming languages: Javascript C, C++, Python, SQL ",
+                            "Familiarity with Git's distributed version control",
                             "Excellent communication skills, both written and verbal",
                             "Adept facilitator of group discussion and negotiations ",
-                            "Strong analytical and troubleshooting capabilities"];
+                            "Strong analytical and troubleshooting capabilities",
+                            "Eager contributor of production focused efforts"];
 
     return(qualifications.map((qualification)=>
             <ListGroup className="Qualifications"><li className="Qualification-bullet" >{qualification.toString()}</li></ListGroup>
