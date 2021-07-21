5aa28d549b77131b37dfc94f3128609a243261b1
kelliewatson <kelliewatson@gmail.com>
2021-07-21 12:30:25 -0700

    updated email

--- src/ContactInfo.js
+++ src/ContactInfo.js
@@ -1,5 +1,5 @@
 import './style/Contact-info.css'
 function ContactInfo(){
-    return(<div><p className="Email">kellie.watsonxyz@gmail.com</p></div>);
+    return(<div><p className="Email">kellie@watsonxyz.com</p></div>);
 }
 export default ContactInfo;
