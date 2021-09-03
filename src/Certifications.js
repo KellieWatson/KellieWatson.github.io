22b9190801d1d70c4dba40ff5dc33d12c1a14d34
kelliewatson <kelliewatson@gmail.com>
2021-09-03 11:17:47 -0700

    added certification completion date and modified layout to accommodate for added information

--- src/Certifications.js
+++ src/Certifications.js
@@ -1,18 +1,19 @@
-import {Card} from "react-bootstrap";
+import {Card, Col, Row} from "react-bootstrap";
 import './style/Certifications.css';
 function Certifications(){
-    const certifications = [ {
-        certification : "Oracle Certified Associate, Java SE 8 Programmer",
+    const certifications = [         {
+        certification : "Oracle Certified Professional, Java SE 8 Programmer",
         issueBy:"Oracle",
-        completionDate : Date.parse("May 17, 2020").toString(),
-        expirationDate : Date.parse("May 17, 2022").toString(),
+        completionDate : "2020",
+        expirationDate : "June 14, 2022",
     },
         {
-            certification : "Oracle Certified Professional, Java SE 8 Programmer",
-            issueBy:"Oracle",
-            completionDate : Date.parse("June 14, 2020").toString(),
-            expirationDate : Date.parse("June 14, 2022").toString(),
-        }]
-    return(certifications.map(certificate => <Card className="Certification-card"><Card.Title className="Certificate-title">{certificate.certification}</Card.Title></Card>))
+        certification : "Oracle Certified Associate, Java SE 8 Programmer",
+        issueBy:"Oracle",
+        completionDate : "2020",
+        expirationDate : "May 17, 2022",
+    }]
+    return(certifications.map(certificate => <Card className="Certification-card"><Row><Col xs={2} className="Completion-date">{certificate.completionDate}</Col>
+        <Col><Card.Title className="Certificate-title">{certificate.certification}</Card.Title></Col></Row></Card>))
 }
 export default Certifications;
